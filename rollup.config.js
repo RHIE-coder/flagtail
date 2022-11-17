9da2555526062eaec472abf601b3e772761e31f6
rhie-coder <quotia72@naver.com>
2022-11-18 00:39:44 +0900

    fix: rollup config - to support Commonjs and ES6 at the same time

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,53 @@
 import commonjs from '@rollup/plugin-commonjs'
 import babel, { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+const input = ["src/index.js"];
 
-export default {
-    input: './src/index.js',
-    output: [
-        {
-            file: './build/bundle.es5.js',
-            format: 'es',
-        }
-    ],
-    plugins: [
-        commonjs(),
-        getBabelOutputPlugin({ presets: ['@babel/preset-env'] }),
-    ]
-}
+export default [
+    {
+        input,
+        plugins: [
+            getBabelOutputPlugin({ presets: ['@babel/preset-env'] }),
+            commonjs(),
+        ],
+        output: [
+            {
+                dir: "dist/esm",
+                format: "esm",
+                exports: "named",
+                sourcemap: true,
+            }
+        ]
+    },
+    // ESM and CJS
+    {
+        input,
+        plugins: [nodeResolve(), commonjs()],
+        output: [
+            {
+                dir: "dist/cjs",
+                format: "cjs",
+                exports: "named",
+                sourcemap: true,
+            },
+        ],
+    },
+];
+
+
+// export default {
+//     input: './src/index.js',
+//     output: [
+//         {
+//             file: './build/bundle.cjs.js',
+//             format: 'cjs',
+//         }
+//     ],
+//     plugins: [
+//         getBabelOutputPlugin({ presets: ['@babel/preset-env'] }),
+//         // commonjs(),
+//     ]
+// }
 
 // export default [
 //     {
