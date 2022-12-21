7dc0a183db2cb3f34c6da670208cfe5f7f3b5e38
rhie-coder <quotia72@naver.com>
2022-12-21 23:56:31 +0900

    publish: @flagtail/type-flow 0.0.2
     - @fix(import fail) : convert 'cjs', 'esm', 'browser' to 'umd'

--- packages/type-flow/rollup.config.js
+++ packages/type-flow/rollup.config.js
@@ -1,49 +1,18 @@
 import commonjs from '@rollup/plugin-commonjs'
-import { getBabelOutputPlugin } from '@rollup/plugin-babel';
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 const input = ["src/index.js"];
 
 export default [
     {
         input,
         plugins: [
-            getBabelOutputPlugin({ presets: ['@babel/preset-env'] }),
-            commonjs(),
-        ],
-        output: [
-            {
-                dir: "dist/esm",
-                format: "esm",
-                exports: "named",
-                sourcemap: true,
-            }
-        ]
-    },
-    {
-        input,
-        plugins: [
-            getBabelOutputPlugin({ presets: ['@babel/preset-env'] }),
             commonjs(),
         ],
         output: [
             {
                 file: "dist/type-flow.js",
-                format: "esm",
-                exports: "named",
-                sourcemap: false,
+                format: "umd",
+                name: "TypeFlow"
             }
         ]
-    },
-    {
-        input,
-        plugins: [nodeResolve(), commonjs()],
-        output: [
-            {
-                dir: "dist/cjs",
-                format: "cjs",
-                exports: "named",
-                sourcemap: true,
-            },
-        ],
     },
 ];
