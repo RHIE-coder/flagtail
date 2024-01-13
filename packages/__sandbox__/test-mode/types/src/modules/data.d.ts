2b1b0d546dbf05b8a7d260b6c7a4392fcaccf7c2
rhie-coder <quotia72@naver.com>
2024-01-14 00:55:53 +0900

    fix: sandbox and examples

--- packages/__sandbox__/test-mode/types/src/modules/data.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class User {
-    name: string;
-    constructor(name: string);
-}
-export default function call(): void;
--- packages/__sandbox__/test-mode/types/src/modules/math.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function add(n1: number, n2: number): number;
-export declare function mul(n1: number, n2: number): number;
